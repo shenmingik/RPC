2a00bcf5ee22a5a6d7e84bdf159303cdf285334a
ik-ubuntu <2107810343@qq.com>
2021-04-05 00:00:50 +0800

    test protobuf more

--- test/protobuf/main.cpp
+++ test/protobuf/main.cpp
@@ -6,6 +6,27 @@ using namespace ik;
 
 int main()
 {
+    LoginResponse login_rsp;
+    ErrorMsg *error_msg = login_rsp.mutable_error();
+    error_msg->set_error(1);
+    error_msg->set_error_msg("login error");
+
+    GetFriendListResponse friend_rsp;
+    ErrorMsg *friend_msg = friend_rsp.mutable_error();
+    friend_msg->set_error(1);
+    friend_msg->set_error_msg("get friend list error");
+
+    User *user1 = friend_rsp.add_friendlists();
+    user1->set_name("zhang san");
+    user1->set_age(20);
+    user1->set_sex(User::man);
+
+    cout << "has friends: " << friend_rsp.friendlists_size() << endl;
+}
+
+#if 0
+int main()
+{
     LoginRequest req;
     req.set_name("zhangsan");
     req.set_password("123456");
@@ -31,3 +52,4 @@ int main()
 
     return 0;
 }
+#endif
