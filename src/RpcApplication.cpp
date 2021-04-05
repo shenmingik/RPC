ef65e8891e938c6e360b50152ddea803060cfd03
ik-ubuntu <2107810343@qq.com>
2021-04-05 17:16:05 +0800

    define class  rpc configure

--- src/RpcApplication.cpp
+++ src/RpcApplication.cpp
@@ -1,7 +1,41 @@
 #include "RpcApplication.hpp"
 
+#include <muduo/base/Logging.h>
+#include <iostream>
+#include <unistd.h>
+#include <string>
+
+using namespace muduo;
+using namespace std;
+
 void RpcApplication::init(int argc, char **argv)
 {
+    if (argc < 2)
+    {
+        LOG_FATAL << "format: command -i <configfile>";
+    }
+
+    int choose = 0;
+    string config_file;
+    while (choose = getopt(argc, argv, "i:") != -1)
+    {
+        switch (choose)
+        {
+        case 'i':
+            config_file = optarg;
+            break;
+        case '?':
+            LOG_FATAL << " format: command -i <configfile>";
+            break;
+        case ':':
+            LOG_FATAL << " format: command -i <configfile>";
+            break;
+        default:
+            break;
+        }
+    }
+
+    //加载配置文件
 }
 
 RpcApplication::RpcApplication()
---
+++ src/RpcConfigure.cpp
@@ -0,0 +1,7 @@
+#include "RpcConfigure.hpp"
+
+//加载配置文件
+void RpcConfigure::load_configure(const char *config_file) {}
+
+//查询配置项信息
+string RpcConfigure::find_load(string &key) {}
